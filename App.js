dc3ef74669050faed126463bc7ec1ba9d70e75cf
Agustin Carnevale <agustincarnevale@Agustins-MacBook-Pro.local>
2020-04-03 21:40:21 -0300

    config in separate file

--- App.js
+++ App.js
@@ -4,20 +4,11 @@ import SignupForm from './src/components/SignupForm'
 import SigninForm from './src/components/SigninForm'
 import Spacer from './src/components/Spacer'
 import firebase from 'firebase'
+import firebaseConfig from './src/firebaseConfig'
 
 const App = ()=> {
 
   useEffect(()=>{
-    const firebaseConfig = {
-      apiKey: "AIzaSyAlwmWHiz5K4IGf_jSk9maiyvpP3mMv-C0",
-      authDomain: "one-time-password-app-60476.firebaseapp.com",
-      databaseURL: "https://one-time-password-app-60476.firebaseio.com",
-      projectId: "one-time-password-app-60476",
-      storageBucket: "one-time-password-app-60476.appspot.com",
-      messagingSenderId: "967862860232",
-      appId: "1:967862860232:web:a01efe86a1a7acbf7fe8fd",
-      measurementId: "G-BJXEPED8QT"
-    };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
   },[])
